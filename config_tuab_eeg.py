1bcf418b4ef0d8e8ac5657fed8930049a70fc690
gemeinl <l.gemein@gmail.com>
2023-01-12 09:02:48 +0000

    update

--- config_tuab_eeg.py
+++ config_tuab_eeg.py
@@ -1,15 +1,12 @@
-from sys import path
 from pathlib import Path
 import mne
 
 study_name = "age-prediction-benchmark"
 
-# On drago
-N_JOBS = 80
-bids_root = Path("/storage/store2/data/TUAB-healthy-bids-bv")
-#deriv_root = Path("/storage/store3/derivatives/TUAB-healthy-bids2")
-deriv_root = Path("/storage/store3/derivatives/TUAB-healthy-bids3")
-subjects_dir = Path('/storage/store/data/camcan-mne/freesurfer')
+N_JOBS = 30
+bids_root = Path("/home/jovyan/bids/tuab/")
+deriv_root = Path("/home/jovyan/bids/tuab_pre/")
+subjects_dir = Path('/home/jovyan/freesurfer/')
 
 source_info_path_update = {'processing': 'autoreject',
                            'suffix': 'epo'}
